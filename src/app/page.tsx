51e7b83640762fa88ef66d2124c3b017abcda121
Md. Talim <talimmohammad116@gmail.com>
2025-09-07 22:28:26 +0530

    Initialize styles, colors, and fonts according to style guide
    - Initialized shadcn
    - Updated globals.css with new theme variables and styles.
    - Modified layout.tsx to use new font imports and updated metadata.
    - Enhanced page.tsx to display a heading.
    - Created utils.ts for class name merging utility.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 const HomePage = () => {
-  return <main>AuraNow</main>;
+  return (
+    <main>
+      <h1 className="font-grotesque text-3xl">How's the sky looking today?</h1>
+    </main>
+  );
 };
 
 export default HomePage;
